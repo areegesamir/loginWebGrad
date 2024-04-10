7199d23aaffeeea6d469029e18025110b4d6a54f
areegesamir <138718044+areegesamir@users.noreply.github.com>
2024-04-10 22:44:01 +0200

    Add files via upload

---
+++ dashboard.jsx
@@ -0,0 +1,78 @@
+<div style={{ width: 1298, height: 708, position: 'relative', background: 'white', borderRadius: 10, overflow: 'hidden' }}>
+    <div style={{ width: 191, height: 208, left: 101, top: 45, position: 'absolute', background: 'linear-gradient(208deg, rgba(7, 1, 40, 0.42) 0%, rgba(98.17, 87.66, 140.25, 0.06) 44%, rgba(98.17, 87.66, 140.25, 0) 100%)', boxShadow: '0px 4px 10px rgba(86, 87, 122, 0.20)', borderRadius: 20, border: '1px rgba(47.41, 38.69, 100.94, 0.12) solid' }} />
+    <div style={{ width: 191, height: 208, left: 310, top: 45, position: 'absolute', background: 'linear-gradient(210deg, rgba(7, 1, 40, 0.42) 5%, rgba(98.17, 87.66, 140.25, 0.05) 51%, rgba(98.17, 87.66, 140.25, 0) 100%)', boxShadow: '0px 4px 10px rgba(86, 87, 122, 0.20)', borderRadius: 20, border: '1px rgba(47.41, 38.69, 100.94, 0.12) solid' }} />
+    <div style={{ width: 300, height: 227, left: 745, top: 419, position: 'absolute', background: 'linear-gradient(221deg, rgba(47.41, 38.69, 100.94, 0.12) 3%, rgba(47.41, 38.69, 100.94, 0.03) 27%, rgba(47.41, 38.69, 100.94, 0) 52%)', boxShadow: '0px 3px 10px rgba(47.41, 38.69, 100.94, 0.60)', borderRadius: 20, border: '1px rgba(47.41, 38.69, 100.94, 0.12) solid' }} />
+    <div style={{ width: 191, height: 208, left: 521, top: 45, position: 'absolute', background: 'linear-gradient(206deg, rgba(7, 1, 40, 0.42) 5%, rgba(98.17, 87.66, 140.25, 0.04) 58%, rgba(98.17, 87.66, 140.25, 0) 100%)', boxShadow: '0px 4px 10px rgba(86, 87, 122, 0.20)', borderRadius: 20, border: '1px rgba(47.41, 38.69, 100.94, 0.12) solid' }} />
+    <div style={{ width: 80, height: 39, left: 330, top: 178, position: 'absolute', color: '#FF81C5', fontSize: 40, fontFamily: 'Montserrat', fontWeight: '600', lineHeight: 40, wordWrap: 'break-word' }}>120</div>
+    <div style={{ width: 78, height: 43, left: 122, top: 178, position: 'absolute', color: '#FF81C5', fontSize: 40, fontFamily: 'Montserrat', fontWeight: '600', lineHeight: 40, wordWrap: 'break-word' }}>120</div>
+    <div style={{ width: 69, height: 28, left: 544, top: 178, position: 'absolute', color: '#FF81C5', fontSize: 40, fontFamily: 'Montserrat', fontWeight: '600', lineHeight: 40, wordWrap: 'break-word' }}>60</div>
+    <div style={{ width: 146, height: 25, left: 122, top: 123, position: 'absolute', color: '#070128', fontSize: 18, fontFamily: 'Montserrat', fontWeight: '500', textTransform: 'capitalize', lineHeight: 18, wordWrap: 'break-word' }}>Detected falls</div>
+    <div style={{ width: 112, height: 23, left: 330, top: 123, position: 'absolute', color: '#070128', fontSize: 18, fontFamily: 'Montserrat', fontWeight: '500', textTransform: 'capitalize', lineHeight: 18, wordWrap: 'break-word' }}>alerts sent</div>
+    <div style={{ width: 154, height: 24, left: 544, top: 124, position: 'absolute', color: '#070128', fontSize: 18, fontFamily: 'Montserrat', fontWeight: '500', textTransform: 'capitalize', lineHeight: 18, wordWrap: 'break-word' }}>health providers</div>
+    <div style={{ width: 300, height: 460, left: 745, top: 45, position: 'absolute' }}>
+        <div style={{ width: 300, height: 460, left: 0, top: 0, position: 'absolute' }}>
+            <div style={{ width: 300, height: 165.49, left: 0, top: 0, position: 'absolute', background: '#2F2765', boxShadow: '0px 5px 10px rgba(98, 88, 140, 0.42)', borderRadius: 20 }} />
+            <img style={{ width: 80.62, height: 45.03, left: 80.62, top: 0, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: '0 0', opacity: 0.20 }} src="https://via.placeholder.com/81x45" />
+            <img style={{ width: 135.69, height: 88.26, left: 164.31, top: 77.23, position: 'absolute', opacity: 0.20 }} src="https://via.placeholder.com/136x88" />
+            <div style={{ width: 300, height: 273, left: 0, top: 187, position: 'absolute' }}>
+                <div style={{ width: 300, height: 273, left: 0, top: 0, position: 'absolute' }}>
+                    <div style={{ width: 300, height: 165, left: 0, top: 0, position: 'absolute', background: '#2F2765', borderRadius: 20 }} />
+                    <img style={{ width: 80.62, height: 44.90, left: 80.62, top: 0, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: '0 0', opacity: 0.10 }} src="https://via.placeholder.com/81x45" />
+                    <img style={{ width: 284, height: 85, left: 284, top: 188, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: '0 0', opacity: 0.10 }} src="https://via.placeholder.com/284x85" />
+                    <img style={{ width: 135.69, height: 88, left: 164.31, top: 77, position: 'absolute', opacity: 0.10 }} src="https://via.placeholder.com/136x88" />
+                </div>
+                <div style={{ width: 207, height: 38, left: 26, top: 21, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '600', textTransform: 'capitalize', wordWrap: 'break-word' }}>failed alerts</div>
+                <div style={{ width: 72, height: 72, left: 25, top: 69, position: 'absolute' }}>
+                    <div style={{ width: 40.15, height: 26.31, left: 21, top: 27, position: 'absolute', color: 'white', fontSize: 14, fontFamily: 'Manrope', fontWeight: '600', wordWrap: 'break-word' }}>25%</div>
+                    <div style={{ width: 72, height: 72, left: 0, top: 0, position: 'absolute' }}>
+                        <div style={{ width: 72, height: 72, left: 0, top: 0, position: 'absolute', borderRadius: 9999, border: '4px white solid' }} />
+                        <div style={{ width: 72, height: 72, left: 72, top: 72, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: '0 0', borderRadius: 9999, border: '4px #FF81C5 solid' }} />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style={{ width: 252, height: 35, left: 27, top: 25, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '600', textTransform: 'capitalize', wordWrap: 'break-word' }}>fall detection accuracy</div>
+        <div style={{ width: 69, height: 67.61, left: 25, top: 75.81, position: 'absolute' }}>
+            <div style={{ width: 38.48, height: 24.70, left: 15.92, top: 22.10, position: 'absolute', color: 'white', fontSize: 14, fontFamily: 'Manrope', fontWeight: '600', wordWrap: 'break-word' }}>75%</div>
+            <div style={{ width: 69, height: 67.61, left: 0, top: 0, position: 'absolute' }}>
+                <div style={{ width: 69, height: 67.61, left: 0, top: 0, position: 'absolute', borderRadius: 9999, border: '4px white solid' }} />
+                <div style={{ width: 69, height: 67.61, left: 0, top: 0, position: 'absolute', borderRadius: 9999, border: '4px #FF81C5 solid' }} />
+            </div>
+        </div>
+    </div>
+    <img style={{ width: 42, height: 44, left: 322, top: 70, position: 'absolute' }} src="https://via.placeholder.com/42x44" />
+    <img style={{ width: 42, height: 44, left: 117, top: 70, position: 'absolute' }} src="https://via.placeholder.com/42x44" />
+    <img style={{ width: 42, height: 44, left: 537, top: 70, position: 'absolute' }} src="https://via.placeholder.com/42x44" />
+    <div style={{ width: 231, height: 38, left: 760, top: 441, position: 'absolute', color: '#2F2765', fontSize: 18, fontFamily: 'Montserrat', fontWeight: '600', textTransform: 'capitalize', lineHeight: 18, letterSpacing: 0.50, wordWrap: 'break-word' }}>communication center</div>
+    <div style={{ width: 26, height: 0, left: 765, top: 479, position: 'absolute', border: '2px #2F2765 solid' }}></div>
+    <img style={{ width: 278, height: 168, left: 774, top: 487, position: 'absolute' }} src="https://via.placeholder.com/278x168" />
+    <div style={{ width: 224, height: 708, left: 1074, top: 0, position: 'absolute', background: 'linear-gradient(186deg, #070128 0%, rgba(89.47, 79.84, 130.63, 0.16) 37%, rgba(255, 128.99, 197.03, 0.10) 100%)' }} />
+    <div style={{ width: 86, height: 82, left: 1145, top: 36, position: 'absolute', background: 'rgba(7, 1, 40, 0.72)', borderRadius: 9999 }} />
+    <img style={{ width: 486, height: 230, left: 1074, top: -91, position: 'absolute', borderTopLeftRadius: 20 }} src="https://via.placeholder.com/486x230" />
+    <img style={{ width: 223, height: 995, left: 1075, top: 0, position: 'absolute' }} src="https://via.placeholder.com/223x995" />
+    <div style={{ width: 198, height: 20, left: 1093, top: 239, position: 'absolute', color: '#070128', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '500', textTransform: 'capitalize', wordWrap: 'break-word' }}>view generated alerts</div>
+    <div style={{ width: 226, height: 39, left: 1075, top: 267, position: 'absolute', background: 'linear-gradient(98deg, #62588C 0%, rgba(98.17, 87.66, 140.25, 0.42) 100%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 2 }} />
+    <div style={{ width: 106, height: 22, left: 1093, top: 276, position: 'absolute', color: 'white', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '500', textTransform: 'capitalize', wordWrap: 'break-word' }}>fall reports</div>
+    <div style={{ width: 156, height: 23, left: 1093, top: 320, position: 'absolute', color: '#070128', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '500', textTransform: 'capitalize', wordWrap: 'break-word' }}>user management</div>
+    <div style={{ width: 186, height: 42, left: 1112, top: 140, position: 'absolute', background: 'white', borderTopLeftRadius: 30, borderTopRightRadius: 30 }} />
+    <div style={{ width: 135, height: 22, left: 1149, top: 150, position: 'absolute', color: '#070128', fontSize: 18, fontFamily: 'Montserrat', fontWeight: '500', textTransform: 'capitalize', lineHeight: 18, wordWrap: 'break-word' }}>ahmed </div>
+    <div style={{ width: 70, height: 705, left: 1, top: 0, position: 'absolute', background: 'linear-gradient(180deg, #070128 0%, rgba(89.47, 79.84, 130.63, 0.12) 25%, rgba(255, 128.99, 197.03, 0.15) 100%)' }} />
+    <img style={{ width: 78, height: 589, left: -6, top: 140, position: 'absolute' }} src="https://via.placeholder.com/78x589" />
+    <div style={{ width: 68, height: 47, left: 18, top: 140, position: 'absolute', background: 'white', borderTopLeftRadius: 20, borderTopRightRadius: 20 }} />
+    <img style={{ width: 70, height: 140, left: 1, top: 0, position: 'absolute', borderTopLeftRadius: 20, borderTopRightRadius: 20 }} src="https://via.placeholder.com/70x140" />
+    <img style={{ width: 28, height: 28, left: 28, top: 150, position: 'absolute' }} src="https://via.placeholder.com/28x28" />
+    <img style={{ width: 28, height: 27, left: 28, top: 198, position: 'absolute' }} src="https://via.placeholder.com/28x27" />
+    <img style={{ width: 28, height: 28, left: 28, top: 245, position: 'absolute' }} src="https://via.placeholder.com/28x28" />
+    <div style={{ width: 611, height: 362, left: 100, top: 291, position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
+        <div style={{ width: 611, height: 362, left: 0, top: 0, position: 'absolute' }}>
+            <div style={{ width: 611, height: 362, left: 0, top: 0, position: 'absolute', background: 'linear-gradient(180deg, rgba(98, 88, 140, 0.42) 0%, rgba(255, 255, 255, 0) 100%)', borderRadius: 29, border: '1px rgba(11.75, 2.08, 71.19, 0.12) solid' }} />
+        </div>
+        <div style={{ width: 119.52, height: 20, left: 25.76, top: 24, position: 'absolute', color: '#070128', fontSize: 20, fontFamily: 'Inter', fontWeight: '600', textTransform: 'capitalize', lineHeight: 34, wordWrap: 'break-word' }}>live steam</div>
+    </div>
+    <div style={{ width: 106, height: 18, left: 423, top: 361, position: 'absolute', color: 'black', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word' }}>camera 2</div>
+    <div style={{ width: 106, height: 18, left: 128, top: 361, position: 'absolute', color: 'black', fontSize: 15, fontFamily: 'Montserrat', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word' }}>camera 1</div>
+    <div style={{ width: 271, height: 190, left: 124, top: 381, position: 'absolute', background: 'white' }} />
+    <div style={{ width: 271, height: 190, left: 417, top: 381, position: 'absolute', background: 'white' }} />
+    <div style={{ width: 98, height: 14, left: 128, top: 552, position: 'absolute', color: '#070128', fontSize: 13, fontFamily: 'Montserrat', fontWeight: '400', textDecoration: 'underline', lineHeight: 34, wordWrap: 'break-word' }}>view full screen</div>
+    <div style={{ width: 98, height: 14, left: 423, top: 552, position: 'absolute', color: '#070128', fontSize: 13, fontFamily: 'Montserrat', fontWeight: '400', textDecoration: 'underline', lineHeight: 34, wordWrap: 'break-word' }}>view full screen</div>
+</div>
